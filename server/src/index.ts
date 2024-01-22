c826e8b456a4b9d8a512a2b3981828ab238cd673
push-ebx <nikita.keyn@yandex.ru>
2024-01-23 00:43:20 +0300

    start template with docker, postgresql, express and prisma

---
+++ server/src/index.ts
@@ -0,0 +1,77 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
+
+dotenv.config();
+
+const prisma = new PrismaClient()
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+app.get('/feed', async (req, res) => {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true }
+  })
+  res.json(posts)
+})
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  })
+  res.json(post)
+})
+
+app.post(`/user`, async (req, res) => {
+  const result = await prisma.user.create({
+    data: { ...req.body },
+  })
+  res.json(result)
+})
+
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  res.json(result)
+})
+
+app.put('/post/publish/:id', async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.update({
+    where: { id: Number(id) },
+    data: { published: true },
+  })
+  res.json(post)
+})
+
+app.delete(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.delete({
+    where: { id: Number(id) },
+  })
+  res.json(post)
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
