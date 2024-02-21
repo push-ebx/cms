48d13dd17ca98dd87492ee8c6f37b7b7ff0b2a04
push-ebx <nikita.keyn@yandex.ru>
2024-02-21 23:38:31 +0300

    поменял свич для смены темы

--- client/src/features/toggle-theme/index.tsx
+++ client/src/features/toggle-theme/index.tsx
@@ -1,23 +1,42 @@
 import { MoonIcon, SunIcon } from "@/shared/ui/icons";
-import { Switch } from "@nextui-org/react";
+import { useSwitch, VisuallyHidden } from "@nextui-org/react";
 import { useDarkMode } from "@/shared/lib/useDarkMode.tsx";
+import { useEffect } from "react";
 
 export const ToggleTheme = () => {
+  const {
+    Component,
+    slots,
+    isSelected,
+    getBaseProps,
+    getInputProps,
+    getWrapperProps
+  } = useSwitch();
   const [_, setTheme] = useDarkMode();
 
+  useEffect(() => {
+    isSelected ? setTheme('light') : setTheme('dark');
+  }, [isSelected]);
+
   return (
-    <Switch
-      onValueChange={value => value ? setTheme('light') : setTheme('dark')}
-      defaultSelected
-      size="sm"
-      color="primary"
-      thumbIcon={({ isSelected, className }) =>
-        isSelected ? (
-          <SunIcon className={className} />
-        ) : (
-          <MoonIcon className={className} />
-        )
-      }
-    />
-  );
+    <div className="flex flex-col gap-2">
+      <Component {...getBaseProps()}>
+        <VisuallyHidden>
+          <input {...getInputProps()} />
+        </VisuallyHidden>
+        <div
+          {...getWrapperProps()}
+          className={slots.wrapper({
+            class: [
+              "w-8 h-8",
+              "flex items-center justify-center",
+              "rounded-md bg-default-100"
+            ],
+          })}
+        >
+          {isSelected ? <SunIcon/> : <MoonIcon/>}
+        </div>
+      </Component>
+    </div>
+  )
 };
